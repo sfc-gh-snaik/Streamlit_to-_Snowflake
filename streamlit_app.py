3bb8cfcd81f02acd6fed5ae849954b712cb6175b
sfc-gh-snaik <109994415+sfc-gh-snaik@users.noreply.github.com>
2023-03-08 21:58:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,11 @@ import streamlit
 import pandas
 import snowflake.connector
 
-# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-# my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT * from MYTABLE")
-# my_data_rows = my_cur.fetchall()
-# streamlit.dataframe(my_data_rows)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from MYTABLE")
+my_data_rows = my_cur.fetchall()
+streamlit.dataframe(my_data_rows)
 
 streamlit.title("Shivali Naik")
 
