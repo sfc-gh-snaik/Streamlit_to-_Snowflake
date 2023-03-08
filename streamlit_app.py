230daab45aca9c5a7995033f9466f33508a52858
sfc-gh-snaik <109994415+sfc-gh-snaik@users.noreply.github.com>
2023-03-08 23:28:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,44 @@
-import streamlit
-import pandas
+# import streamlit
+# import pandas
+# import snowflake.connector
+
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT * from mytable")
+# my_data_rows = my_cur.fetchall()
+# streamlit.dataframe(my_data_rows)
+
+# streamlit.title("Shivali Naik")
+
+
+# streamlit_app.py
+
+import streamlit as st
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from mytable")
-my_data_rows = my_cur.fetchall()
-streamlit.dataframe(my_data_rows)
+# Initialize connection.
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
 
-streamlit.title("Shivali Naik")
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
 
 
 
